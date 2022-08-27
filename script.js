6673b511849efcc1647572043e36e7d184df1cf9
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-08-27 00:34:28 -0300

    navegacao-tabs

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//eventos
+document.documentElement.classList.add("js");
+const animaisLista = document.querySelectorAll(".animais-imagens li");
+const animaisConteudo = document.querySelectorAll(".animais-descricao section");
+
+function ativaTabs(index) {
+    animaisConteudo.forEach((item) => {
+        item.classList.remove("ativo");
+    });
+    animaisConteudo[index].classList.add("ativo");
+}
+
+if (animaisLista.length && animaisConteudo.length) {
+    animaisLista.forEach((img, index) => {
+        img.addEventListener("click", () => {
+            ativaTabs(index);
+        })
+    });
+}
