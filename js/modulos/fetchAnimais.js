535f60867f7c9fa0b103dfee72e9d9ccc0a52c3c
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2023-01-18 18:54:02 -0300

    fix: fetchUrl

--- js/modulos/fetchAnimais.js
+++ js/modulos/fetchAnimais.js
@@ -1,28 +1,25 @@
-import initNumeros from './numeros.js';
+import initNumeros from "./numeros.js";
 
-export default function InitFetch(){
-  async function fetchAnimais(){
-    const response = await fetch('/animaisapi.json');
+export default function InitFetch() {
+  async function fetchAnimais() {
+    const response = await fetch("./animaisapi.json");
     const animaisJson = await response.json();
     const grid = document.querySelector(".numeros-grid");
 
-    animaisJson.forEach((item)=>{
+    animaisJson.forEach((item) => {
       renderAnimais(createAnimais(item));
-    })
-    function createAnimais(animal){
-      const div = document.createElement('li');
-      div.classList.add('numero-animal');
-      div.innerHTML= `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`
-      return div
+    });
+    function createAnimais(animal) {
+      const div = document.createElement("li");
+      div.classList.add("numero-animal");
+      div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`;
+      return div;
     }
-    function renderAnimais(li){
+    function renderAnimais(li) {
       grid.appendChild(li);
     }
-  
-  initNumeros();
+
+    initNumeros();
   }
   fetchAnimais();
 }
-
-
-
